4efac334865cfce274fd2620099a0f753f2a11f0
ponomarenko.pavel.web@gmail.com <ponomarenko.pavel.web@gmail.com>
2022-11-30 17:21:20 +0200

    update from spck editor

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const template = '##h2 title \n Some text here! [link Text](https://google.com "primary") without to see how <br/> it works <br/> out inthe parser ya know. \n [Other link](https://github.com/ "secondary") And what about some more **bold** *italic* text and using ###h3 Image \n ![Placehold.it 200x200 image](https://img.freepik.com/premium-photo/image-planet-outer-space-mixed-media-elements-image-furnished-by-nasa_641298-3434.jpg?w=1060)';
+const template = '##h2 title \n Some text here! [link Text](https://google.com "primary") without to see how <br/> it works <br/> out inthe parser ya know. \n [Other link](https://github.com/ "secondary") And what about some more **bold** *italic* text and using ###h3 Image \n ![image-placeholder](https://img.freepik.com/premium-photo/image-planet-outer-space-mixed-media-elements-image-furnished-by-nasa_641298-3434.jpg?w=1060)';
 
 const output = document.createElement('div');
 output.classList.add('output');
@@ -14,7 +14,7 @@ code.innerText = parseTemplate(template);
 document.body.appendChild(code);
 
 function parseTemplate(md) {
-	
+	// titles
   md = md.replace(/[\#]{6}(.+)/g, '<h6>$1</h6>');
   md = md.replace(/[\#]{5}(.+)/g, '<h5>$1</h5>');
   md = md.replace(/[\#]{4}(.+)/g, '<h4>$1</h4>');
@@ -27,7 +27,6 @@ function parseTemplate(md) {
   
   //links
   md = md.replace(/[\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g, '<a href="$2" class="$4">$1</a>');
-  console.log(md)
   
   //font styles
   md = md.replace(/[\*\_]{2}([^\*\_]+)[\*\_]{2}/g, '<b>$1</b>');
